e063b455546c72380c81d81e4db2f2bbb76b8a98
Jared Tessensohn <jtessensohn@yahoo.com>
2021-01-12 14:38:49 -0600

    created madlib.py

---
+++ madlib.py
@@ -0,0 +1,6 @@
+print("Please fill in the blanks below: \n___(name)___'s favorite subject in school is ___(subject)___.")
+
+name = input("What is name?")
+subject = input("What is subject?")
+
+print(str(name) + "'s favorite subject in school is " + str(subject))
