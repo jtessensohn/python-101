ad06b7f0a2187cb8c0055216533662faeaafb03f
Jared Tessensohn <jtessensohn@yahoo.com>
2021-01-13 13:55:29 -0600

    messing around in tipcalc

--- tip_calc.py
+++ tip_calc.py
@@ -1,8 +1,15 @@
-total = int(input('Total bill amount? $'))
-level = str(input('Level of service? (good, fair, bad) '))
-if level == "good":
-    print("$" + str(total + (total *.20)))
-if level == "fair":
-    print("$" + str(total + (total *.15)))
-if level == "bad":
-    print("$" + str(total + (total *.10)))
+while True:
+    try:
+        total = int(input('Total bill amount? $'))
+        level = input('Level of service? (good, fair, bad) ')
+        if level == "good":
+            print("$" + str(total + (total *.20)))
+        if level == "fair":
+            print("$" + str(total + (total *.15)))
+        if level == "bad":
+            print("$" + str(total + (total *.10)))
+    except ValueError:
+        print("Please rate based on what is in the parentheses")
+
+    
+    
